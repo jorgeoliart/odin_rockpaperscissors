4443a96aeb78091827d334a8b5f77f3de2de2951
jorgeoliart <jorgeoliart.gmail.com>
2023-05-18 19:27:47 -0500

    Add return to getComputerChoice function

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function getComputerChoice() {
     let threeOptions = ['Rock', 'Paper', 'Scissor'];
     let computerSelectionNumber = Math.floor(Math.random() * 3);
     let computerSelectionString = threeOptions[computerSelectionNumber];
+    return computerSelectionString;
 } 
 
