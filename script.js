bf883a1f2c9621c806afee44239fb355fba4d9e3
jorgeoliart <jorgeoliart.gmail.com>
2023-05-18 19:21:27 -0500

    Create a variable that uses the random generated integer to get an element from array

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     let threeOptions = ['Rock', 'Paper', 'Scissor'];
     let computerSelectionNumber = Math.floor(Math.random() * 3);
-    
+    let computerSelectionString = threeOptions[computerSelectionNumber];
 } 
 
