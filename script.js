a46afd114562c0bec764e5c57a91893d3b93db01
jorgeoliart <jorgeoliart.gmail.com>
2023-05-23 21:24:06 -0500

    Create function to get player choice in each round with prompt

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function getComputerChoice() {
-    let threeOptions = ['Rock', 'Paper', 'Scissor'];
+    let threeOptions = ['rock', 'paper', 'scissor'];
     let computerSelectionNumber = Math.floor(Math.random() * 3);
     let computerSelectionString = threeOptions[computerSelectionNumber];
     return computerSelectionString;
 } 
 
+function getPlayerChoice () {
+    let playerSelectionString = prompt("Make your choice: Rock, Paper, or Scissors");
+    playerSelectionString = playerSelectionString.toLowerCase();
+    return playerSelectionString;
+}
