c06622c45e4fa3ff19ece2bb664a4e2e9554d648
Jorge Oliart <jorgeoliart@users.noreply.github.com>
2023-07-15 08:18:49 -0500

    Create function that runs the game

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 function getComputerChoice() {
     let threeOptions = ['rock', 'paper', 'scissors'];
     let computerSelectionNumber = Math.floor(Math.random() * 3);
-    let computerSelectionString = threeOptions[computerSelectionNumber];
+    let computerSelectionString = threeOptions[computerSelectionNumer];
     return computerSelectionString;
-} 
+}
 
+// Function that runs the game
+function game() {
+}
